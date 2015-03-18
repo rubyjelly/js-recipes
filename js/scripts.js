094ff4b9899c1dfa8eb5a2096ea3fa17e416f259
rubyjelly <crystalafinch@gmail.com>
2015-03-17 18:30:24 -0700

    Allow for demo buttons next to tab labels

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,20 @@ $(document).ready(function() {
 		var labels = document.getElementsByClassName("tab-label");
 		for (var i=0; i<labels.length; i++) {
 			var label = labels[i],
-				labelLink = labels[i].childNodes[0];
-			label.setAttribute("aria-expanded", "false");
-			labelLink.onclick = function toggleAccordion(evt) {
+				labelParent = label.parentNode;
+			labelParent.setAttribute("aria-expanded", "false");
+			label.onclick = function toggleAccordion(evt) {
 				var parent = this.parentNode,
 					panel = parent.nextSibling.nextSibling;
 				evt.stopPropagation();
 				if (panel.style.display == "block") {
 					panel.style.display = "none";
+					panel.setAttribute("aria-hidden", "true");
+					parent.setAttribute("aria-expanded", "false");
 				} else {
-					panel.style.display = "block"
+					panel.style.display = "block";
+					panel.setAttribute("aria-hidden", "false");
+					parent.setAttribute("aria-expanded", "true");
 				}
 				return false;
 			}
